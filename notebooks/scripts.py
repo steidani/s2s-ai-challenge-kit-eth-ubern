2861890c3e12f0e4dd637e7ad9f936fc9c9256ab
Aaron Spring <aaron.spring@mpimet.mpg.de>
2021-05-24 14:01:01 +0000

    uncomment renku, assert func, cleanup

--- notebooks/scripts.py
+++ notebooks/scripts.py
@@ -1,9 +1,11 @@
 import xarray as xr
 import pandas as pd
+import numpy as np
 
 cache_path = '../data'
 
-def add_valid_time_from_forecast_reference_time_and_lead_time(benchmark, init_dim='forecast_time'):
+
+def add_valid_time_from_forecast_reference_time_and_lead_time(forecast, init_dim='forecast_time'):
     """Creates valid_time(forecast_time, lead_time).
     
     lead_time: pd.Timedelta
@@ -12,19 +14,18 @@ def add_valid_time_from_forecast_reference_time_and_lead_time(benchmark, init_di
     times = xr.concat(
         [
             xr.DataArray(
-                benchmark[init_dim] + lead,
+                forecast[init_dim] + lead,
                 dims=init_dim,
-                coords={init_dim: benchmark[init_dim]},
+                coords={init_dim: forecast[init_dim]},
             )
-            for lead in benchmark.lead_time
+            for lead in forecast.lead_time
         ],
         dim="lead_time",
         join="inner",
         compat="broadcast_equals",
     )
-    benchmark = benchmark.assign_coords(valid_time=times)
-    return benchmark
-
+    forecast = forecast.assign_coords(valid_time=times)
+    return forecast
 
 
 def make_probabilistic(ds, tercile_edges, member_dim='realization', mask=None):
@@ -39,7 +40,7 @@ def make_probabilistic(ds, tercile_edges, member_dim='realization', mask=None):
         bn = bn.mean(member_dim)
         an = an.mean(member_dim)
         n = n.mean(member_dim)
-    ds_p = xr.concat([bn,n,an],'category').assign_coords(category=['below normal','normal','above normal'])
+    ds_p = xr.concat([bn, n, an],'category').assign_coords(category=['below normal', 'normal', 'above normal'])
     if mask is not None:
         ds_p = ds_p.where(mask)
     if 'tp' in ds_p.data_vars:
@@ -54,7 +55,7 @@ def make_probabilistic(ds, tercile_edges, member_dim='realization', mask=None):
 def print_RPS_per_year(preds):
     """Returns pd.Dataframe of RPS per year."""
     # similar verification_RPSS.ipynb
-    # as scorer bot
+    # as scorer bot but returns a score for each year
     import xarray as xr
     import xskillscore as xs
     import pandas as pd
@@ -88,15 +89,15 @@ def print_RPS_per_year(preds):
         # dirty fix
         rpss = rpss.clip(-1, 1)
 
-    mask = xr.ones_like(rpss.isel(lead_time=0,drop=True)).reset_coords(drop=True).t2m
+    mask = xr.ones_like(rpss.isel(lead_time=0, drop=True)).reset_coords(drop=True).t2m
     boundary_tropics = 30
     mask = xr.concat([mask.where(mask.latitude > boundary_tropics),
                     mask.where(np.abs(mask.latitude) <= boundary_tropics),
                     mask.where((mask.latitude < -boundary_tropics) & (mask.latitude > -60))],'area')
     mask = mask.assign_coords(area=['northern_extratropics', 'tropics', 'southern_extratropics'])
-    mask.name='area'
+    mask.name = 'area'
 
-    mask = mask.where(rpss.t2m.isel(lead_time=0,drop=True).notnull())
+    mask = mask.where(rpss.t2m.isel(lead_time=0, drop=True).notnull())
     
     # weighted area mean
     weights = np.cos(np.deg2rad(np.abs(mask.latitude)))
@@ -106,15 +107,51 @@ def print_RPS_per_year(preds):
     # final score
     scores = rpss.weighted(weights).mean('latitude').mean('longitude')
     # spatially weighted score averaged over lead_times and variables to one single value
-    scores = scores.to_array().mean(['lead_time','variable'])
+    scores = scores.to_array().mean(['lead_time', 'variable'])
     return scores.to_dataframe('RPS')
 
+
 def assert_predictions_2020(preds_test):
+    """Check the variables, coordinates and dimensions of 2020 predictions."""
     # is dataset
-    
+    assert isinstance(preds_test, xr.Dataset)
+
     # has both vars: tp and t2m
+    assert 'tp' in preds_test.data_vars
+    assert 't2m' in preds_test.data_vars
     
-    # sizes
+    ## coords
+    # forecast_time
+    d = pd.date_range(start='2020-01-02', freq='7D', periods=53)
+    forecast_time = xr.DataArray(d, dims='forecast_time', coords={'forecast_time':d})
+    assert (forecast_time == preds_test['forecast_time']).all()
+
+    # longitude
+    lon = np.arange(0., 360., 1.5)
+    longitude = xr.DataArray(lon, dims='longitude', coords={'longitude': lon})
+    assert (longitude == preds_test['longitude']).all()
     
-    # coords
-    pass
+    # latitude
+    lat = np.arange(90., -90.1, 1.5)
+    latitude = xr.DataArray(lat, dims='latitude', coords={'latitude': lat})
+    assert (latitude == preds_test['latitude']).all()
+    
+    # lead_time
+    lead = [pd.Timedelta(f'{i} d') for i in [14, 28]]
+    lead_time = xr.DataArray(lead, dims='lead_time', coords={'lead_time': lead})
+    assert (lead_time == preds_test['lead_time']).all()
+    
+    # category
+    cat = np.array(['below normal', 'normal', 'above normal'], dtype='<U12')
+    category = xr.DataArray(cat, dims='category', coords={'category': cat})
+    assert (category == preds_test['category']).all()
+    
+    # size
+    from dask.utils import format_bytes
+    size_in_MB = float(format_bytes(preds_test.nbytes).replace(' MiB',''))
+    assert size_in_MB > 100
+    assert size_in_MB < 200
+    
+    # no other dims
+    assert set(preds_test.dims) - {'category', 'forecast_time', 'latitude', 'lead_time', 'longitude'} == set()
+    
