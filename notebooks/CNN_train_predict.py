3d627d45cc2a167ddf1fed9bbf917f01a97aada3
HoratN <nina.horat@kit.edu>
2021-10-27 18:43:39 +0200

    refactor code and use seed to create multiple predictions

--- notebooks/CNN_train_predict.py
+++ notebooks/CNN_train_predict.py
@@ -4,8 +4,8 @@
 
 train and predict
 """
-#%%
-   
+
+         
 import tensorflow.keras as keras
 import tensorflow as tf
 from tensorflow.keras.models import Model
@@ -31,44 +31,12 @@ from helper_ml_data import pad_earth, add_valid_time_single, single_prediction, 
 import warnings
 warnings.simplefilter("ignore")
 #%%
-### set seed to make shuffling in datagenerator reproducible
-np.random.seed(42)
-
+#set env random seeds 
 import os
 os.environ['TF_CUDNN_DETERMINISTIC'] = 'true'
 os.environ['TF_DETERMINISTIC_OPS'] = 'true'
-
-import random as rn
-#set all random seeds
 os.environ['PYTHONHASHSEED'] = '0'
-#np.random.seed(1)
-rn.seed(1254)
-tf.random.set_seed(89)
-#%%
-#params:
-smoothing = 0.6
-output_dims = 8
-input_dims = 32
-basis_rad = 16
-n_bins = 3
 
-# hyper-parameters for CNN
-dropout_rate = 0.4
-hidden_nodes = [10] #they tried different architectures
-bs=32
-ep  = 20#20
-
-sigma = 9# guassian filtering: best sigma for years 2017/2018
-
-#train domain:
-input_lat = slice(81,34)#32
-input_lon = slice(49,97)#32
-
-output_lat = slice(63,52)#8          #slice(69,46)#16
-output_lon = slice(67,78)#8          #slice(61,85) #16
-    
-#%%
-#load training data
 def get_data(varlist, path_data):
 # ## Hindcast
     hind_2000_2019 = load_data(data = 'hind_2000-2019', aggregation = 'biweekly', path = path_data,var_list = varlist)#.isel(lead_time = lead_input)#['tp','t2m','sm20','sst'])#,'lsm', 'msl', var_list = ['tp','t2m'])#path_data)
@@ -88,145 +56,177 @@ def get_data(varlist, path_data):
     
     return hind_2000_2019, obs_2000_2019, obs_2000_2019_terciled, mask
 
-path_data = 'local_n'#'server'##['../../../../Data/s2s_ai/data', '../../../../Data/s2s_ai']
-hind_2000_2019, obs_2000_2019, obs_2000_2019_terciled, mask = get_data(['t2m', 'tp'], path_data)
+def train_patch(fct_train_patch, verif_train_patch, basis, clim_probs, bs, v, n_xy, n_basis):
 
+    # ### create batches
+    print('create batches')
+    dg_train = DataGenerator1(fct_train_patch, verif_train_patch, 
+                             basis = basis, clim_probs = clim_probs,
+                             batch_size=bs, load=True)
+    
+    print('finished creating batches')
 
-#%%
-for v in ['t2m', 'tp']:
-    for lead_time in [0,1]:
-#v='t2m'
+    # CNN: slightly adapted from Scheuerer et al 2020.
+            
+    inp_imgs = Input(shape=(dg_train[0][0][0].shape[1], dg_train[0][0][0].shape[2],dg_train[0][0][0].shape[3],)) #fcts4121,240,
+    inp_basis = Input(shape=(n_xy,n_basis)) #basis
+    inp_cl = Input(shape=(n_xy,n_bins,)) #climatology
+    
+    c = Conv2D(4, (3,3), activation='elu')(inp_imgs)
+    c = MaxPooling2D((2,2))(c)
+    #c = Conv2D(8, (3,3), activation='elu')(c)
+    #c = MaxPooling2D((2,2))(c)
+    x = Flatten()(c)
+    for h in hidden_nodes: 
+        x = Dropout(dropout_rate)(x)
+        x = Dense(h, activation='elu')(x)
+    x = Dense(n_bins*n_basis, activation='elu')(x)
+    x = Reshape((n_bins,n_basis))(x)
+    z = Dot(axes=2)([inp_basis, x])     # Tensor product with basis functions
+    #z = Add()([z, inp_cl])              # Add (log) probability anomalies to log climatological probabilities 
+    out = Activation('softmax')(z)
+    
+    cnn = Model(inputs=[inp_imgs, inp_basis, inp_cl], outputs=out)
+    #cnn.summary()
+    
+    cnn.compile(loss= keras.losses.CategoricalCrossentropy(label_smoothing = smoothing), metrics=['accuracy'], optimizer=keras.optimizers.Adam(1e-4))#'adam')#my_loss_rps
+    
+    hist =cnn.fit(dg_train, epochs=ep, shuffle = False)   
 
-# 2 bi-weekly `lead_time`: week 3-4
-#lead_time = 0#hind_2000_2019.lead_time[0]
-#lead_output = 0#obs_2000_2019.lead_time[0]
+    return cnn
 
-        # ### create datasets
-        fct_train = hind_2000_2019[[v]].isel(lead_time = lead_time)
-        if v == 'tp':
-            fct_train = xr.where(fct_train < 0, 0, fct_train)
-          
-        verif_train = obs_2000_2019_terciled[v].isel(lead_time = lead_time)
-        verif_train = verif_train.where(mask[v].isel(lead_time = lead_time).notnull())
-        
-        # preprocess input: compute and standardize features
-        fct_train = preprocess_input(fct_train, v, path_data, lead_time)
-        
-        #up to here is preprocessing of global data
+def train_predict_onevar_onelead(v, lead_time, seed, hind_2000_2019, obs_2000_2019, obs_2000_2019_terciled, mask, path_data):
+    # ### create datasets
+    fct_train = hind_2000_2019[[v]].isel(lead_time = lead_time)
+    if v == 'tp':
+        fct_train = xr.where(fct_train < 0, 0, fct_train)
+      
+    verif_train = obs_2000_2019_terciled[v].isel(lead_time = lead_time)
+    verif_train = verif_train.where(mask[v].isel(lead_time = lead_time).notnull())
     
-        
-        #%%
-        #train data:
-        fct_train_patch = fct_train.sel(longitude = input_lon, latitude = input_lat)
-        verif_train_patch = verif_train.sel(longitude = output_lon, latitude = output_lat)
-        #%%
-        
-        # #### compute basis
-        basis, lats, lons, n_xy, n_basis = get_basis(verif_train_patch.isel(category = 0), basis_rad)
-        ##the smaller you choose the radius of the basis functions, the more memory needs to be allocated (a lot more!)
-        
-        clim_probs = np.log(1/3)*np.ones((n_xy,n_bins))
-        
-         #%%
-        
-        def train_patch(fct_train_patch, verif_train_patch, basis, clim_probs, bs, v):
+    # preprocess input: compute and standardize features
+    fct_train = preprocess_input(fct_train, v, path_data, lead_time)
+    
+    #up to here is preprocessing of global data
 
-            # ### create batches
-            print('create batches')
-            dg_train = DataGenerator1(fct_train_patch, verif_train_patch, 
-                                     basis = basis, clim_probs = clim_probs,
-                                     batch_size=bs, load=True)
-            
-            print('finished creating batches')
-        
-            # CNN: slightly adapted from Scheuerer et al 2020.
-                    
-            inp_imgs = Input(shape=(dg_train[0][0][0].shape[1], dg_train[0][0][0].shape[2],dg_train[0][0][0].shape[3],)) #fcts4121,240,
-            inp_basis = Input(shape=(n_xy,n_basis)) #basis
-            inp_cl = Input(shape=(n_xy,n_bins,)) #climatology
-            
-            c = Conv2D(4, (3,3), activation='elu')(inp_imgs)
-            c = MaxPooling2D((2,2))(c)
-            #c = Conv2D(8, (3,3), activation='elu')(c)
-            #c = MaxPooling2D((2,2))(c)
-            x = Flatten()(c)
-            for h in hidden_nodes: 
-                x = Dropout(dropout_rate)(x)
-                x = Dense(h, activation='elu')(x)
-            x = Dense(n_bins*n_basis, activation='elu')(x)
-            x = Reshape((n_bins,n_basis))(x)
-            z = Dot(axes=2)([inp_basis, x])     # Tensor product with basis functions
-            #z = Add()([z, inp_cl])              # Add (log) probability anomalies to log climatological probabilities 
-            out = Activation('softmax')(z)
-            
-            cnn = Model(inputs=[inp_imgs, inp_basis, inp_cl], outputs=out)
-            #cnn.summary()
-            
-            cnn.compile(loss= keras.losses.CategoricalCrossentropy(label_smoothing = smoothing), metrics=['accuracy'], optimizer=keras.optimizers.Adam(1e-4))#'adam')#my_loss_rps
-            
-            hist =cnn.fit(dg_train, epochs=ep, shuffle = False)   
-        
-            return cnn
-        
-        #### train model for each var and lead time
-        cnn = train_patch(fct_train_patch, verif_train_patch, basis, clim_probs, bs, v)
+    #%%
+    #train data:
+    fct_train_patch = fct_train.sel(longitude = input_lon, latitude = input_lat)
+    verif_train_patch = verif_train.sel(longitude = output_lon, latitude = output_lat)
+    #%%
     
-        cnn.save(f'CNN_patch_lead{lead_time}_{v}_{ep}')#creates a new folder with model details
-        #cnn_restored = keras.models.load_model("CNN_patch_2")
-        
-        #%%           
-        ##############################################################################
-        #predict for training data
-        
-        #vars to get back dataset coords after prediction
-        global_lats = fct_train.latitude
-        global_lons = fct_train.longitude
-        lead_output_coords = fct_train.lead_time
-        
+    # #### compute basis
+    basis, lats, lons, n_xy, n_basis = get_basis(verif_train_patch.isel(category = 0), basis_rad)
+    ##the smaller you choose the radius of the basis functions, the more memory needs to be allocated (a lot more!)
     
-        #####pad the earth by the required amount
-        hind_padded = pad_earth(fct_train, input_dims, output_dims)
-        
-        #select years to predict for
-        fct_predict = hind_padded#.sel(forecast_time = slice('2017','2018'))
-        
-        ### predict globally using the trained local model    
-        prediction = slide_predict(fct_predict, input_dims, output_dims, cnn, basis, clim_probs)
-        
-        da = add_coords(prediction, fct_predict, global_lats, global_lons, lead_output_coords)
-        da.to_dataset(name = v).to_netcdf(f'../submissions/global_prediction_{v}_lead{lead_time}_raw_allyears.nc')
+    clim_probs = np.log(1/3)*np.ones((n_xy,n_bins))
     
-        #smoothed predictions
-        # patchwise predictions result in edgy forecasts, use gaussian filtering to smooth the predictions
-        # one additional hyperparameter to optimize: sigma
-        prediction_smoothed = sp.ndimage.gaussian_filter(prediction, sigma=(0,sigma,sigma,0), order=0)
-        
-        da = add_coords(prediction_smoothed, fct_predict, global_lats, global_lons, lead_output_coords)
-        da.to_dataset(name = v).to_netcdf(f'../submissions/global_prediction_{v}_lead{lead_time}_smooth_allyears.nc')
+     #%%
+    #### train model for each var and lead time
+    cnn = train_patch(fct_train_patch, verif_train_patch, basis, clim_probs, bs, v, n_xy, n_basis)
+
+    cnn.save(f'CNN_patch_lead{lead_time}_{v}_seed{seed}_{ep}')
+    #creates a new folder with model details
+    #cnn_restored = keras.models.load_model("CNN_patch_2")
     
-         
-        #%%%
-        ###############################################################################
-        ##read 2020 data and create prediction for 2020 data.
-                
-        fct_2020 = load_data(data = 'forecast_2020', aggregation = 'biweekly', path = path_data,var_list = [v]).isel(lead_time = lead_time)
-        fct_2020 = clean_coords(fct_2020)[[v]]
-        if v == 'tp':
-            fct_2020 = xr.where(fct_2020 < 0, 0, fct_2020)
-        # preprocess input: compute and standardize features
-        fct_2020 = preprocess_input(fct_2020, v, path_data, lead_time)
+    #%%           
+    ##############################################################################
+    #predict for training data
+    
+    #vars to get back dataset coords after prediction
+    global_lats = fct_train.latitude
+    global_lons = fct_train.longitude
+    lead_output_coords = fct_train.lead_time
+    
+
+    #####pad the earth by the required amount
+    hind_padded = pad_earth(fct_train, input_dims, output_dims)
+    
+    #select years to predict for
+    fct_predict = hind_padded#.sel(forecast_time = slice('2017','2018'))
+    
+    ### predict globally using the trained local model    
+    prediction = slide_predict(fct_predict, input_dims, output_dims, cnn, basis, clim_probs)
+    
+    da = add_coords(prediction, fct_predict, global_lats, global_lons, lead_output_coords)
+    da.to_dataset(name = v).to_netcdf(f'../submissions/{seed}/global_prediction_{v}_lead{lead_time}_raw_allyears.nc')
+
+    #smoothed predictions
+    # patchwise predictions result in edgy forecasts, use gaussian filtering to smooth the predictions
+    # one additional hyperparameter to optimize: sigma
+    prediction_smoothed = sp.ndimage.gaussian_filter(prediction, sigma=(0,sigma,sigma,0), order=0)
+    
+    da = add_coords(prediction_smoothed, fct_predict, global_lats, global_lons, lead_output_coords)
+    da.to_dataset(name = v).to_netcdf(f'../submissions/{seed}/global_prediction_{v}_lead{lead_time}_smooth_allyears.nc')
+
+     
+    #%%%
+    ###############################################################################
+    ##read 2020 data and create prediction for 2020 data
+            
+    fct_2020 = load_data(data = 'forecast_2020', aggregation = 'biweekly', path = path_data,var_list = [v]).isel(lead_time = lead_time)
+    fct_2020 = clean_coords(fct_2020)[[v]]
+    if v == 'tp':
+        fct_2020 = xr.where(fct_2020 < 0, 0, fct_2020)
+    # preprocess input: compute and standardize features
+    fct_2020 = preprocess_input(fct_2020, v, path_data, lead_time)
+    
+    fct_2020_padded = pad_earth(fct_2020, input_dims, output_dims)
+    
+    pred_2020 = slide_predict(fct_2020_padded, input_dims, output_dims, cnn, basis, clim_probs)
+    
+    #save raw predictions
+    da = add_coords(pred_2020, fct_2020, global_lats, global_lons, lead_output_coords)
+    da.to_dataset(name = v).to_netcdf(f'../submissions/{seed}/global_prediction_{v}_lead{lead_time}_2020_raw.nc')
+    
+    pred_2020_smoothed = sp.ndimage.gaussian_filter(pred_2020, sigma=(0,sigma,sigma,0), order=0)
+    #convert to dataset and save
+    da_smooth = add_coords(pred_2020_smoothed, fct_2020, global_lats, global_lons, lead_output_coords)
+    da_smooth.to_dataset(name = v).to_netcdf(f'../submissions/{seed}/global_prediction_{v}_lead{lead_time}_2020_smooth.nc')
+    return 
+
+if __name__ == '__main__':
+ 
+    #%%
+    #params:
+    smoothing = 0.6
+    output_dims = 8
+    input_dims = 32
+    basis_rad = 16
+    n_bins = 3
+    
+    # hyper-parameters for CNN
+    dropout_rate = 0.4
+    hidden_nodes = [10] #they tried different architectures
+    bs=32
+    ep  = 20
+    
+    sigma = 9# guassian filtering: best sigma for years 2017/2018
+    
+    #train domain:
+    input_lat = slice(81,34)#32
+    input_lon = slice(49,97)#32
+    
+    output_lat = slice(63,52)#8          #slice(69,46)#16
+    output_lon = slice(67,78)#8          #slice(61,85) #16
         
-        fct_2020_padded = pad_earth(fct_2020, input_dims, output_dims)
+    #%%
+    #load training data
+    path_data = 'server'#'local_n'##['../../../../Data/s2s_ai/data', '../../../../Data/s2s_ai']
+    hind_2000_2019, obs_2000_2019, obs_2000_2019_terciled, mask = get_data(['t2m', 'tp'], path_data)
+    
+    
+    #%%
+    for seed in range(10,60,10):
         
-        pred_2020 = slide_predict(fct_2020_padded, input_dims, output_dims, cnn, basis, clim_probs)
+        #use this to create an ensemble of predictions
+        tf.random.set_seed(seed)
+        np.random.seed(seed)
+        import random as rn
+        rn.seed(seed)
+            
+        for v in ['t2m', 'tp']:
+            for lead_time in [0,1]:
         
-        #save raw predictions
-        da = add_coords(pred_2020, fct_2020, global_lats, global_lons, lead_output_coords)
-        da.to_dataset(name = v).to_netcdf(f'../submissions/global_prediction_{v}_lead{lead_time}_2020_raw.nc')
-        
-        pred_2020_smoothed = sp.ndimage.gaussian_filter(pred_2020, sigma=(0,sigma,sigma,0), order=0)
-        #convert to dataset and save
-        da_smooth = add_coords(pred_2020_smoothed, fct_2020, global_lats, global_lons, lead_output_coords)
-        da_smooth.to_dataset(name = v).to_netcdf(f'../submissions/global_prediction_{v}_lead{lead_time}_2020_smooth.nc')
-        
+                train_predict_onevar_onelead(v, lead_time, seed, hind_2000_2019, obs_2000_2019, obs_2000_2019_terciled, mask, path_data)
 
